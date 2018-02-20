391618be2d6b7ac5a95cdba3a899a7c11096ca19
Leony <31887229+brleony@users.noreply.github.com>
2018-02-20 01:51:03 +0100

    downloaded csv file

---
+++ Homework/Week3/convertCSV2JSON.py
@@ -0,0 +1,16 @@
+# Name: Leony Brok
+# Student number: 10767215
+# Data source: https://opendata.cbs.nl/#/CBS/nl/dataset/82814NED/table?ts=1519084165675
+
+INPUT_CSV = 'donardata.csv'
+OUTPUT_JSON = 'donordata.txt'
+
+if __name__ == "__main__":
+
+    # open csv input
+    with open(INPUT_CSV) as input_file:
+        #
+
+    # write the JSON to disk
+    with open(OUTPUT_JSON, ) as output_file:
+        #
