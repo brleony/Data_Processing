f105c621770a2827bab45d5577603e7889e73737
Leony <31887229+brleony@users.noreply.github.com>
2018-02-23 14:20:01 +0100

    added axis styles

--- Homework/Week3/barchart.js
+++ Homework/Week3/barchart.js
@@ -12,9 +12,10 @@ if (document.addEventListener) {
 function createChart() {
 
   d3.json("sundata.json", function(data) {
+    var extractedData = extractData(data);
 
-    // save hours of sunshine per week
-    var sunshineHours = getSunshineHours(data);
+    var weeks = extractedData[0];
+    var sunshineHours = extractedData[1];
 
     // determine svg attributes
     var barWidth = 15,
@@ -29,37 +30,50 @@ function createChart() {
       .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    drawBars(chart, sunshineHours, barWidth, height);
+    // scale sunshine hours
+    var y = d3.scale.linear()
+        .domain([d3.max(sunshineHours, function(d) { return d; }), 0])
+        .range([0, height]);
+
+    var x = d3.scale.ordinal()
+        .domain(weeks.map(function(d) { return d; }))
+        .rangeRoundBands([0, width], .1);
+
+    drawBars(chart, sunshineHours, barWidth, height, y, x);
+
+    drawAxes(chart, y, x, height);
   });
 }
 
-function getSunshineHours(data) {
+
+function extractData(data) {
+  var weeks = [];
   var sunshineHours = [];
   var sunshineWeek = 0;
   var numWeekdays = 7;
 
   // save sum of hours of sunshine per week
   for (var i = 0, len = data.length; i < len; i++) {
+
     sunshineWeek += data[i].sunshineHours;
+
     if (i % numWeekdays == numWeekdays - 1) {
+      weeks.push(data[i].date);
       sunshineHours.push(Math.round(sunshineWeek));
       sunshineWeek = 0;
     }
   }
 
   if (sunshineWeek > 0) {
+    weeks.push(data[i].date);
     sunshineHours.push(Math.round(sunshineWeek));
   }
 
-  return sunshineHours;
+  return [weeks, sunshineHours];
 }
 
-function drawBars(chart, sunshineHours, barWidth, height) {
 
-  // scale sunshine hours
-  var y = d3.scale.linear()
-      .domain([d3.max(sunshineHours, function(d) { return d; }), 0])
-      .range([0, height]);
+function drawBars(chart, sunshineHours, barWidth, height, y, x) {
 
   // enter data
   var bar = chart.selectAll("g")
@@ -69,14 +83,34 @@ function drawBars(chart, sunshineHours, barWidth, height) {
 
   // add bars to chart
   bar.append("rect")
-    .attr("y", function(d) { return y(d) - 10; })
+    .attr("y", function(d) { return y(d) })
     .attr("height", function(d) { return height - y(d); })
     .attr("width", barWidth - 1);
 
-  // add labels to bars
-  bar.append("text")
-    .attr("y", function(d) { return y(d) - 3; })
-    .attr("x", barWidth / 2)
-    .attr("dy", ".75em")
-    .text(function(d) { return d; });
+  // // add labels to bars
+  // bar.append("text")
+  //   .attr("y", function(d) { return y(d) - 3; })
+  //   .attr("x", barWidth / 2)
+  //   .attr("dy", ".75em")
+  //   .text(function(d) { return d; });
+}
+
+
+function drawAxes(chart, y, x, height) {
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left");
+
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+
+  chart.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  chart.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
 }
