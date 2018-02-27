f895230b9ee4e0545853f8ec24609ad1ccf5512f
Leony <31887229+brleony@users.noreply.github.com>
2018-02-27 21:35:56 +0100

    created files for scatterplot

---
+++ Homework/Week4/scatterplot.js
@@ -0,0 +1,16 @@
+/*
+Name: Leony Brok
+Student number: 10767215
+*/
+
+// wait until DOM has loaded
+if (document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", function() {
+        main();
+  });
+}
+
+// log greeting
+function main() {
+    console.log("hello world");
+}
