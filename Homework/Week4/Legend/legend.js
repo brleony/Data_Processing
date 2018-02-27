168e7a38c23b00db96f24889adc8ed4c8b215d04
Leony <31887229+brleony@users.noreply.github.com>
2018-02-27 13:15:27 +0100

    added main

--- Homework/Week4/Legend/legend.js
+++ Homework/Week4/Legend/legend.js
@@ -1,4 +1,17 @@
 /*
 Name: Leony Brok
 Student number: 10767215
+
+To create the legend you will append a rectangle and a text element for each entry to an SVG element using the D3 library.
 */
+
+// wait until DOM has loaded
+if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', function() {
+        main();
+  });
+}
+
+function main() {
+  console.log("hihi");
+}
