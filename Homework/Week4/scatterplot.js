6a6dbe281af0f1c8c473d86691f6bc332d0cd171
Leony <31887229+brleony@users.noreply.github.com>
2018-02-28 16:14:15 +0100

    added queue

--- Homework/Week4/scatterplot.js
+++ Homework/Week4/scatterplot.js
@@ -6,11 +6,35 @@ Student number: 10767215
 // wait until DOM has loaded
 if (document.addEventListener) {
     document.addEventListener("DOMContentLoaded", function() {
-        main();
+        createScatterplot();
   });
 }
 
-// log greeting
-function main() {
-    console.log("hello world");
+// create scatterplot
+function createScatterplot() {
+
+    // get data from csv files
+    d3.queue()
+        .defer(d3.csv, "ginidata.csv")
+        .defer(d3.tsv, "tourismdata.csv")
+        .await(analyze);
+
+    // determine svg attributes
+    var margin = {top: 20, right: 30, bottom: 30, left: 40},
+        height = 600 - margin.top - margin.bottom,
+        width = 800 - margin.left - margin.right;
+
+    // set chart height and width
+    var scatterplot = d3.select(".scatterplot")
+        .attr("height", height + margin.top + margin.bottom)
+        .attr("width", width + margin.left + margin.right)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+}
+
+function analyze(error, gini, tourism) {
+    if(error) { console.log(error); }
+
+    console.log(gini);
+    console.log(tourism);
 }
