6a04097df5abfc5d10b8c1a0c4bf8826954166a7
Leony <31887229+brleony@users.noreply.github.com>
2018-03-01 01:08:11 +0100

    made week 5 files

---
+++ Homework/Week5/d3line.js
@@ -0,0 +1,20 @@
+/*
+Name: Leony Brok
+Student number: 10767215
+*/
+
+// wait until DOM has loaded
+if (document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", function() {
+
+      // // get data from csv files
+      // d3.queue()
+      //     .defer(d3.csv, "data1.csv")
+      //     .defer(d3.csv, "data2.csv")
+      //     .await(createLineGraph);
+  });
+}
+
+function createLineGraph (error, data1, data2) {
+  console.log("Under Construction");
+}
