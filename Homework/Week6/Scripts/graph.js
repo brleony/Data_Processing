4f21327c715dbed6941fa0110556111638602744
Leony <31887229+brleony@users.noreply.github.com>
2018-03-12 16:43:24 +0100

    updated

--- Homework/Week6/Scripts/graph.js
+++ Homework/Week6/Scripts/graph.js
@@ -2,3 +2,23 @@
 * Name: Leony Brok
 * Student number: 10767215
 **/
+
+// wait until DOM has loaded
+if (document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", function() {
+
+      // // get data from csv files
+      // d3.queue()
+      //     .defer(d3.csv, "ginidata.csv")
+      //     .defer(d3.csv, "tourismdata.csv")
+      //     .defer(d3.csv, "gdpdata.csv")
+      //     .await(createGraph);
+
+      createGraph();
+  });
+}
+
+function createGraph () {
+
+   console.log("HI!");
+};
