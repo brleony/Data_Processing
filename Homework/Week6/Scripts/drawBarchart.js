dedddc7552bcf7eca6e6333c33c4a4ad457abe5f
Leony <31887229+brleony@users.noreply.github.com>
2018-03-24 02:01:25 +0100

    added function sort button

--- Homework/Week6/Scripts/drawBarchart.js
+++ Homework/Week6/Scripts/drawBarchart.js
@@ -5,10 +5,13 @@
 * Draws a bar chart.
 */
 
+/*
+* Draws a barchart.
+*/
 function drawBarchart() {
 
   // Determine svg attributes.
-  margin = {top: 20, right: 30, bottom: 180, left: 80},
+  margin = {top: 20, right: 30, bottom: 200, left: 80},
       barWidth = 30,
       height = 500 - margin.top - margin.bottom,
       width = barWidth * (d3.keys(religion[province()][year()]).length);
@@ -20,10 +23,10 @@ function drawBarchart() {
     .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+  // Scale y and x.
   y = d3.scaleLinear()
       .domain([100, 0])
       .range([0, height]);
-
   x = d3.scaleBand()
       .domain(d3.keys(religion[province()][year()]))
       .range([0, width]);
@@ -45,25 +48,47 @@ function drawBarchart() {
   drawXAxis(barchart, height, x);
 };
 
+/*
+* Update barchart with new province.
+*/
 function updateBarchart() {
 
   var barchart = d3.select(".barchart");
 
+  // Update bars with new data.
+  var data = religion[province()][year()];
   var bar = barchart.selectAll("rect")
-      .data(d3.values(religion[province()][year()]))
-      .transition()
-        .duration(700)
-        .attr("y", function(d) { return y(d); })
-        .attr("height", function(d) { return height - y(d); });
+      .data(d3.entries(data));
 
+  // Update x domain with new data.
+  x.domain(d3.keys(religion[province()][year()]));
+
+  // Create transition.
+  var transition = barchart.transition().duration(800);
+  var delay = function(d, i) { return i * 50; };
+
+  // Transition bars.
+  transition.selectAll("rect")
+    .delay(delay)
+    .attr("x", function (d) {return x(d.key); })
+    .attr("y", function(d) { return y(d.value); })
+    .attr("height", function(d) { return height - y(d.value); });
+
+  // Transition x axis.
+  transition.select(".x.axis")
+    .call(xAxis)
+   .selectAll("g")
+    .delay(delay);
+
+  // Update barchart title.
   document.getElementById("provincename").textContent=province();
 };
 
 /*
 * Sort bars in barchart in descending order.
 *
-* Based on example by James Curley
-* https://bl.ocks.org/jalapic/acb4b8b6523e73394c86/454da2974578bcc9b4bb4090cf005cd2fa1a3ef6
+* Based on example by Mike Bostock
+* https://bl.ocks.org/mbostock/3885705
 */
 function sortBars() {
 
@@ -71,16 +96,28 @@ function sortBars() {
   var barchart = d3.select(".barchart");
   var bars = barchart.selectAll("rect");
 
-  // Sort bars in descending order.
-  bars.sort(function(a, b) { return d3.descending(a, b); })
-			.transition()
-  			.delay(function(d, i) { return i * 50; })
-  			.duration(700)
-  			.attr("transform", function(d, i) { return "translate(" + i * barWidth + ", 0)"; });
+  var data = d3.entries(religion[province()][year()]);
 
-  var xAxis = d3.select(".x.axis");
-  xAxis.transition()
-    .duration(700);
+  // Sort data in descending order.
+  var sortedData = data.sort(
+      function(a, b) { return d3.descending(a.value, b.value); });
+
+  x.domain(sortedData.map(function(d) { return d.key; }));
+
+  // Create transition.
+  var transition = barchart.transition().duration(800);
+  var delay = function(d, i) { return i * 50; };
+
+  // Sort bars.
+  transition.selectAll("rect")
+    .delay(delay)
+    .attr("x", function(d) {return x(d.key); });
+
+  // Sort x axis.
+  transition.select(".x.axis")
+    .call(xAxis)
+   .selectAll("g")
+    .delay(delay);
 }
 
 /*
@@ -90,21 +127,21 @@ function drawBars(barchart, barWidth, height, tipBarchart) {
 
     // Enter data.
     var bar = barchart.selectAll("rect")
-        .data(d3.values(religion[province()][year()]))
+        .data(d3.entries(religion[province()][year()]))
         .enter();
 
     // Add bars to chart.
     bar.append("rect")
-        .attr("transform", function(d, i) { return "translate(" + i * barWidth + ", 0)"; })
-        .attr("y", function(d) { return y(d); })
-        .attr("height", function(d) { return height - y(d); })
+        .attr("x", function(d) {return x(d.key)})
+        .attr("y", function(d) { return y(d.value); })
+        .attr("height", function(d) { return height - y(d.value); })
         .attr("width", barWidth - 1)
         .on("mouseover", function(d) {
-            tipBarchart.show(d);
+            tipBarchart.show(d.value);
             d3.select(this).style("fill", "#93B7BE");
         })
         .on("mouseout", function(d) {
-            tipBarchart.hide(d);
+            tipBarchart.hide(d.value);
             d3.select(this).style("fill", "#241557");
         });
 }
@@ -116,7 +153,7 @@ function drawYAxis(barchart, height) {
 
     barchart.append("g")
         .attr("class", "y axis")
-        .call(d3.axisLeft(y))
+        .call(d3.axisLeft(y).tickFormat(function(n) { return n + "%"}))
       .append("text")
         .attr("fill", "#000")
         .attr("transform", "rotate(-90)")
@@ -124,7 +161,7 @@ function drawYAxis(barchart, height) {
         .attr("x", - (height / 2))
         .attr("dy", "0.71em")
         .style("font-weight", "bold")
-        .text("% inwoners");
+        .text("Inwoners");
 }
 
 /*
@@ -132,7 +169,7 @@ function drawYAxis(barchart, height) {
 */
 function drawXAxis(barchart, height, x) {
 
-  var xAxis = d3.axisBottom(x)
+  xAxis = d3.axisBottom(x)
     .tickSize(0);
 
   barchart.append("g")
